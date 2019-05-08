deedd7651433ed3fe4c46c1b054dd2961fc303e4
tomoya-yokota <tomoya-yokota@cybozu.co.jp>
2019-05-08 13:39:56 +0900

    Practice-3

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,62 @@ import ReactDOM from "react-dom";
 import {MyComponent} from "./MyComponent";
 import {FieldGroup, Button, RadioButton, Text} from "@kintone/kintone-ui-component/lib/js/components-react";
 import "@kintone/kintone-ui-component/lib/style";
+import {Provider, Subscribe, Container} from 'unstated';
 
-const app = document.getElementById("app");
+interface State {
+    numOfClicks: number;
+};
+
+class AppContainer extends Container<State> {
+    state = {
+        numOfClicks: 0
+    };
+
+    increment() {
+        console.log(this.state.numOfClicks);
+        const numOfClicks = this.state.numOfClicks + 1;
+        this.setState({numOfClicks});
+    }
+}
+
 const items = [{
     label: "item-1",
     value: "1"
 }];
+
+function App() {
+    return (
+        <Subscribe to={[AppContainer]}>
+            {(container: AppContainer) => (
+                <div>
+                <FieldGroup toggle="expand">
+                    <Button 
+                        text="Click ME" 
+                        type="submit"
+                        onClick={() => container.increment()}
+                    />
+                    <RadioButton 
+                        name="RadioButton"
+                        items={items}
+                        value="1"
+                    />
+                    <Text value={container.state.numOfClicks}/>
+                </FieldGroup>
+                <MyComponent class="mycomponent">
+                    <span>hello!</span>
+                    <span>hello!</span>
+                </MyComponent>
+            </div>
+            )}
+        </Subscribe>
+    )
+}
+const app = document.getElementById("app");
+
 ReactDOM.render(
-    <div>
-        <FieldGroup toggle="expand">
-            <Button 
-                text="Click ME" 
-                type="submit"
-            />
-            <RadioButton 
-                name="RadioButton"
-                items={items}
-                value="1"
-            />
-            <Text />
-        </FieldGroup>
-        <MyComponent class="mycomponent">
-            <span>hello!</span>
-            <span>hello!</span>
-        </MyComponent>
-    </div>, 
+    <Provider>
+        <App />
+    </Provider>
+    , 
     app
 );
